d05a0eb6a7ec85cfa8324f6a5ca6ce188dffcbf4
ulookforward <ihao168@sina.com>
2021-12-16 01:03:55 +0800

    Add files via upload
    提交代码

---
+++ TestGitFunction/TestGitFunction/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestGitFunction
+//
+//  Created by xuxinghao on 2021/12/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestGitFunction/TestGitFunction/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TestGitFunction
+//
+//  Created by xuxinghao on 2021/12/15.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
